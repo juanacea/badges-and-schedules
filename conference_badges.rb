ff1ec30ddc10bc962e1b01e98af1f887f0a8ee89
juanacea <juan.acea93@gmail.com>
2021-01-24 23:00:41 -0500

    commit tree d58c4aeb8ca43790c77b393a735c9df24944b761

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badge_messages = []
+    attendees.each do |attendee|
+        badge_message = "Hello, my name is #{attendee}."
+        badge_messages.push(badge_message)
+    end
+    return badge_messages
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    rooms = [1, 2, 3, 4, 5, 6, 7]
+    count = 0
+    attendees.each do |attendee|
+        current_room = rooms[count]
+        room_message = "Hello, #{attendee}! You'll be assigned to room #{current_room}!"
+        room_assignments.push(room_message)
+        count += 1
+    end
+    return room_assignments
+end
+
+def printer(attendees)
+    return batch_badge_creator(attendees)
+    badge_messages.each do |message|
+        puts #{message}
+    end
+end
